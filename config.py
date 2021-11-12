835a48789d7f467c0f4671425936dda72b0dfea9
VODOLAZZZ <sergeypletnev2004@gmail.com>
2021-11-12 10:37:55 +0300

    Add files

---
+++ config.py
@@ -0,0 +1,129 @@
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.callback_data import CallbackData
+
+TOKEN = '2096892030:AAGczC-gxnMIFbKFymQIvOr2x9R2QqywrZ0'
+
+start_message = 'Привет, {0}, чем я могу вам помочь? Если вы хотите заказать еду, нажмите кнопку "Заказ еды". ' \
+                'Если хотите, чтобы убрали в комнате, нажмите "Уборка комнаты". ' \
+                'Если хотите прочитать новости про отель, нажмите "Новости".'
+
+main_menu_message = start_message
+main_menu_buttons = ['Заказ еды', 'Уборка номера', 'Новости']
+main_menu_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2).add(*main_menu_buttons)
+
+ordering_food_message = 'Выберете и нажмите на одну из кнопок снизу.'
+ordering_food_callback_data = CallbackData('ordering_food', 'button_name')
+ordering_food_inline_buttons = [
+    InlineKeyboardButton('Холодные закуски', callback_data=ordering_food_callback_data.new(button_name='0')),
+    InlineKeyboardButton('Первые блюда', callback_data=ordering_food_callback_data.new(button_name='1')),
+    InlineKeyboardButton('Вторые блюда', callback_data=ordering_food_callback_data.new(button_name='2')),
+    InlineKeyboardButton('Напитки', callback_data=ordering_food_callback_data.new(button_name='3')),
+    InlineKeyboardButton('Сформировать заказ',
+                         callback_data=ordering_food_callback_data.new(button_name='form_an_order')),
+    InlineKeyboardButton('Назад',
+                         callback_data=ordering_food_callback_data.new(button_name='back_to_main_menu'))
+]
+ordering_food_inline_keyboard = InlineKeyboardMarkup(resize_keyboard=True, row_width=1).add(
+    *ordering_food_inline_buttons)
+
+ordering_food_inline_button_back_to_category_menu = InlineKeyboardButton('Назад',
+                                                                         callback_data=ordering_food_callback_data.new(
+                                                                             button_name='back_to_category_menu'))
+
+ordering_food_category_callback_data = CallbackData('ordering_food_category', 'category', 'id')
+ordering_food_message_c0 = 'ХОЛОДНЫЕ ЗАКУСКИ'
+ordering_food_inline_buttons_c0 = [
+    InlineKeyboardButton('Яйцо вареное 1/070 12-00',
+                         callback_data=ordering_food_category_callback_data.new(category='0', id='0')),
+    InlineKeyboardButton('Творог со сметаной 1/100 50-00',
+                         callback_data=ordering_food_category_callback_data.new(category='0', id='1')),
+    InlineKeyboardButton('Сырники 1/070 60-00',
+                         callback_data=ordering_food_category_callback_data.new(category='0', id='2')),
+]
+ordering_food_inline_keyboard_c0 = InlineKeyboardMarkup(resize_keyboard=True, row_width=1).add(
+    *ordering_food_inline_buttons_c0).add(ordering_food_inline_button_back_to_category_menu)
+
+ordering_food_message_c1 = 'ПЕРВЫЕ БЛЮДА'
+ordering_food_inline_buttons_c1 = [
+    InlineKeyboardButton('Борщ красный 1/300 50-00',
+                         callback_data=ordering_food_category_callback_data.new(category='1', id='0')),
+    InlineKeyboardButton('Борщ зеленый 1/301 50-00',
+                         callback_data=ordering_food_category_callback_data.new(category='1', id='1')),
+    InlineKeyboardButton('Рассольник 1/300 60-00',
+                         callback_data=ordering_food_category_callback_data.new(category='1', id='2')),
+]
+ordering_food_inline_keyboard_c1 = InlineKeyboardMarkup(resize_keyboard=True, row_width=1).add(
+    *ordering_food_inline_buttons_c1).add(ordering_food_inline_button_back_to_category_menu)
+
+ordering_food_message_c2 = 'ВТОРЫЕ БЛЮДА'
+ordering_food_inline_buttons_c2 = [
+    InlineKeyboardButton('Солянка по-грузински 1/300 200-00',
+                         callback_data=ordering_food_category_callback_data.new(category='2', id='0')),
+    InlineKeyboardButton('Гуляш /говядина/ 1/100 100-00',
+                         callback_data=ordering_food_category_callback_data.new(category='2', id='1')),
+    InlineKeyboardButton('Филе рыбы Зубатка 1/100 60-00',
+                         callback_data=ordering_food_category_callback_data.new(category='2', id='2')),
+]
+ordering_food_inline_keyboard_c2 = InlineKeyboardMarkup(resize_keyboard=True, row_width=1).add(
+    *ordering_food_inline_buttons_c2).add(ordering_food_inline_button_back_to_category_menu)
+
+ordering_food_message_c3 = 'НАПИТКИ'
+ordering_food_inline_buttons_c3 = [
+    InlineKeyboardButton('Вода 50-00',
+                         callback_data=ordering_food_category_callback_data.new(category='3', id='0')),
+    InlineKeyboardButton('Сок Яблочный 100-00',
+                         callback_data=ordering_food_category_callback_data.new(category='3', id='1')),
+    InlineKeyboardButton('Сок Апельсиновый 100-00',
+                         callback_data=ordering_food_category_callback_data.new(category='3', id='2')),
+    InlineKeyboardButton('Сок Вишневый 100-00',
+                         callback_data=ordering_food_category_callback_data.new(category='3', id='3')),
+    InlineKeyboardButton('Кофе 80-00',
+                         callback_data=ordering_food_category_callback_data.new(category='3', id='4')),
+    InlineKeyboardButton('Чай 60-00',
+                         callback_data=ordering_food_category_callback_data.new(category='3', id='5')),
+]
+ordering_food_inline_keyboard_c3 = InlineKeyboardMarkup(resize_keyboard=True, row_width=1).add(
+    *ordering_food_inline_buttons_c3).add(ordering_food_inline_button_back_to_category_menu)
+
+
+ordering_food_inline_keyboard_c_text = '''ХОЛОДНЫЕ ЗАКУСКИ
+Яйцо вареное 1/070 12-00
+Творог со сметаной 1/100 50-00
+Сырники 1/070 60-00
+
+ПЕРВЫЕ БЛЮДА
+Борщ красный 1/300 50-00
+Борщ зеленый 1/301 50-00
+Рассольник 1/300 60-00
+
+ВТОРЫЕ БЛЮДА
+Солянка по-грузински 1/300 200-00
+Гуляш /говядина/ 1/100 100-00
+Филе рыбы Зубатка 1/100 60-00
+
+НАПИТКИ
+Вода 50-00
+Сок Яблочный 100-00
+Сок Апельсиновый 100-00
+Сок Вишневый 100-00
+Кофе 80-00
+Чай 60-00'''
+
+ordering_food_inline_keyboard_c_text_list = [i.split('\n') for i in ordering_food_inline_keyboard_c_text.split('\n\n')]
+
+# 476343978
+# 616343618
+
+ordering_food_form_an_order_message = 'Ваш заказ {0}, будет доставлен в номер {1}, в течение часа.'
+ordering_food_form_an_order_inline_keyboard = InlineKeyboardMarkup(resize_keyboard=True, row_width=1).add(
+    ordering_food_inline_buttons[-1])
+
+room_cleaning_message = 'Уборка'
+room_cleaning_buttons = ['Назад']
+room_cleaning_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(*room_cleaning_buttons)
+
+news_message = 'новости'
+news_buttons = ['Назад']
+news_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(*news_buttons)
---
+++ main.py
@@ -0,0 +1,130 @@
+import config as cg
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher, FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.utils import executor
+from typing import Union
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+bot = Bot(token=cg.TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+cart = {}
+orders = []
+
+
+class OrderFood(StatesGroup):
+    waiting_for_room_name = State()
+
+
+async def edit_ordering_food_inline_keyboard(card: list):
+    buttons_list = cg.ordering_food_inline_keyboard_c_text_list
+    finale_list = []
+
+
+@dp.message_handler(commands='id')
+async def start(message: types.Message):
+    print(message.from_user.id)
+    await message.answer(f'Ваш айди:{message.from_user.id}')
+
+
+# @dp.message_handler(commands='set_room')
+# async def start(message: types.Message):
+#     room[message.from_user.id] = message.get_args()
+
+
+@dp.message_handler(commands='start')
+async def start(message: types.Message):
+    await message.answer(cg.start_message.format(message.chat.first_name), reply_markup=cg.main_menu_keyboard)
+
+
+@dp.message_handler(Text(equals=[cg.room_cleaning_buttons[-1], cg.news_buttons[-1]]))
+@dp.callback_query_handler(Text(endswith='back_to_main_menu'))
+async def main_menu(cam: Union[types.CallbackQuery, types.Message]):
+    if type(cam) == types.CallbackQuery:
+        await cam.message.edit_reply_markup()
+        await cam.message.answer(cg.main_menu_message.format(cam.from_user.first_name),
+                                 reply_markup=cg.main_menu_keyboard)
+    elif type(cam) == types.Message:
+        await cam.answer(cg.start_message.format(cam.chat.first_name), reply_markup=cg.main_menu_keyboard)
+
+
+@dp.callback_query_handler(cg.ordering_food_callback_data.filter(button_name='back_to_category_menu'))
+@dp.message_handler(Text(equals=cg.main_menu_buttons[0]))
+async def ordering_food(cam: Union[types.CallbackQuery, types.Message]):
+    if type(cam) == types.Message:
+        await cam.answer('Что будете заказывать?', reply_markup=cg.ReplyKeyboardRemove())
+        await cam.answer(cg.ordering_food_message, reply_markup=cg.ordering_food_inline_keyboard)
+    elif type(cam) == types.CallbackQuery:
+        await cam.message.edit_text(cg.ordering_food_message, reply_markup=cg.ordering_food_inline_keyboard)
+        await cam.answer()
+
+
+@dp.callback_query_handler(cg.ordering_food_callback_data.filter(button_name=['0', '1', '2', '3']))
+async def ordering_food_category(call: types.CallbackQuery, callback_data: dict):
+    category = callback_data['button_name']
+    if category == '0':
+        await call.message.edit_text(cg.ordering_food_message_c0, reply_markup=cg.ordering_food_inline_keyboard_c0)
+    elif category == '1':
+        await call.message.edit_text(cg.ordering_food_message_c1, reply_markup=cg.ordering_food_inline_keyboard_c1)
+    elif category == '2':
+        await call.message.edit_text(cg.ordering_food_message_c2, reply_markup=cg.ordering_food_inline_keyboard_c2)
+    elif category == '3':
+        await call.message.edit_text(cg.ordering_food_message_c3, reply_markup=cg.ordering_food_inline_keyboard_c3)
+    await call.answer()
+
+
+@dp.callback_query_handler(cg.ordering_food_category_callback_data.filter())
+async def ordering_food_add_food(call: types.CallbackQuery, callback_data: dict):
+    food_id = callback_data['category'] + callback_data['id']
+    cart[call.from_user.id] = cart.get(call.from_user.id, [])
+    cart[call.from_user.id] += [food_id]
+    await call.answer()
+
+
+@dp.callback_query_handler(cg.ordering_food_callback_data.filter(button_name=['form_an_order']))
+async def ordering_food_form_an_order_enter_room(call: types.CallbackQuery):
+    cart[call.from_user.id] = cart.get(call.from_user.id, [])
+    if cart[call.from_user.id]:
+        await call.message.edit_text('Введите номер в который принесут заказ')
+        await OrderFood.waiting_for_room_name.set()
+        await call.answer()
+    else:
+        await call.answer('Вы ничего не добавили в корзину', show_alert=True)
+
+
+@dp.message_handler(state=OrderFood.waiting_for_room_name)
+async def ordering_food_enter_room_name(message: types.Message, state: FSMContext):
+    if not message.text.isdigit():
+        await message.answer('Вы неправильно указали номер, пожалуйста введите только цифры')
+    else:
+        await state.finish()
+        room = message.text
+        user_id = message.from_user.id
+        cart[user_id] = cart.get(user_id, [])
+        number = len(orders) + 1
+        orders.append({room: cart[user_id]})
+        await bot.send_message(476343978,
+                               f'Ты типо повар, к тебе тут заказ прилетел: номер {number} в номер {room} ({", ".join(orders[-1][room])})')
+        await message.answer(cg.ordering_food_form_an_order_message.format(number, room),
+                             reply_markup=cg.ordering_food_form_an_order_inline_keyboard)
+        cart[user_id] = []
+
+
+@dp.message_handler(Text(equals=cg.main_menu_buttons[1]))
+async def room_cleaning(message: types.Message):
+    await message.answer(cg.room_cleaning_message, reply_markup=cg.room_cleaning_keyboard)
+
+
+@dp.message_handler(Text(equals=cg.main_menu_buttons[2]))
+async def news(message: types.Message):
+    await message.answer(cg.news_message, reply_markup=cg.news_keyboard)
+
+
+async def shutdown(dispatcher: Dispatcher):
+    await dispatcher.storage.close()
+    await dispatcher.storage.wait_closed()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_shutdown=shutdown)
